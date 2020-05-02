bc59e4c1b672bbbb8d8ad90692ab5e58d0538417
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-05-02 09:03:14 +0900

    add mainjs to setTimeout

---
+++ Mystopwatch/js/main.js
@@ -0,0 +1,22 @@
+'user script'
+
+{
+  const timer = document.getElementById('timer');
+  const start = document.getElementById('start');
+  const stop = document.getElementById('stop');
+  const reset = document.getElementById('reset');
+  
+  let startTime
+
+  function countUp(){
+    console.log(Date.now() - startTime);
+
+    setTimeout(()=>{
+      countUp();
+    }, 10);
+  }
+  start.addEventListener('click', ()=>{
+    startTime = Date.now();
+    countUp();
+  });
+}
