dcf5b6d5d4c679d6d01cff9ff226943ee228b197
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-08-21 15:09:34 +0900

    FINISH BingoLesson

--- MyBingo/js/main.js
+++ MyBingo/js/main.js
@@ -16,29 +16,28 @@
     return column;
   }
 
-  const columns = [];
-  columns[0] = createColumn(0);
-  columns[1] = createColumn(1);
-  columns[2] = createColumn(2);
-  columns[3] = createColumn(3);
-  columns[4] = createColumn(4);
-  columns[2][2] = 'FREE';
+  function createColumns() {
+    const columns = [];
+    for (let i = 0; i < 5; i++) {
+      columns[i] = createColumn(i);
+    }
+    columns[2][2] = 'FREE';
 
-  const bingo = [];
-  for (let row = 0; row < 5; row++) {
-    bingo[row] = [];
-    for (let col = 0; col < 5; col++) {
-      bingo[row][col] = columns[col][row]
+    return columns
+  }
+
+  function renderBingo(bingo) {
+    for (let row = 0; row < 5; row++) {
+      const tr = document.createElement('tr');
+      for (let col = 0; col < 5; col++) {
+        const td = document.createElement('td');
+        td.textContent = bingo[col][row];
+        tr.appendChild(td);
+      }
+      document.querySelector('tbody').appendChild(tr);
     }
   }
 
-  for (let row = 0; row < 5; row++) {
-    const tr = document.createElement('tr');
-    for (let col = 0; col < 5; col++) {
-      const td = document.createElement('td');
-      td.textContent = bingo[row][col];
-      tr.appendChild(td);
-    }
-    document.querySelector('tbody').appendChild(tr);
-  }
+  const columns = createColumns();
+  renderBingo(columns);
 }
