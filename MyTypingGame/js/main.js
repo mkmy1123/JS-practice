861ee244f0a5cddb1e7f6ec5fac83cca2ad0fbdf
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-08-23 20:31:36 +0900

    FINISH Typing Game lesson

--- MyTypingGame/js/main.js
+++ MyTypingGame/js/main.js
@@ -8,16 +8,18 @@
     'middle',
     'set',
   ]
-  let word = words[Math.floor(Math.random() * words.length)];
-  let loc = 0;
-  let score = 0;
-  let miss = 0;
+  let word;
+  let loc;
+  let score;
+  let miss;
+  let timeLimit = 3 * 1000;
+  let startTime;
+  let isPlaying = false;
 
   const target = document.getElementById('target');
   const scoreLabel = document.getElementById('score');
   const missLabel = document.getElementById('miss');
-  target.textContent = word;
-
+  const timerLabel = document.getElementById('timer');
 
   function updateTarget() {
     let placeholder = '';
@@ -27,8 +29,53 @@
     target.textContent = placeholder + word.substring(loc);
   }
 
+  function updateTimer() {
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+
+    const timeoutId = setTimeout(() => {
+      updateTimer();
+    }, 10);
+
+    if (timeLeft < 0) {
+      isPlaying = false;
+      clearTimeout(timeoutId);
+      timerLabel.textContent = '0.00';
+      setTimeout(() => {
+        showResult();
+      }, 100);
+
+      target.textContent = 'click to replay';
+    }
+  }
+
+  function showResult() {
+    const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
+    alert(`${score} letters, ${miss} misses, ${accuracy.toFixed(2)}% accuracy!`)
+  }
+
+  window.addEventListener('click', () => {
+    if (isPlaying == true) {
+      return;
+    }
+    isPlaying = true;
+
+    loc = 0;
+    score = 0;
+    miss = 0;
+    scoreLabel.textContent = score;
+    missLabel.textContent = miss;
+    word = words[Math.floor(Math.random() * words.length)];
+
+    target.textContent = word;
+    startTime = Date.now();
+    updateTimer();
+  });
+
   window.addEventListener('keydown', e => {
-    console.log(e.key);
+    if (isPlaying !== true) {
+      return;
+    }
     if (e.key == word[loc]) {
       loc++;
       if (loc === word.length) {
