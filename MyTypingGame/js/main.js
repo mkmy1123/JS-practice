47bc7f45249014e85ead25c957f8998c9dc13e1d
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-08-23 19:55:55 +0900

    implement word transition / Typing Game lesson

--- MyTypingGame/js/main.js
+++ MyTypingGame/js/main.js
@@ -1,7 +1,14 @@
 'use strict';
 
 {
-  const word = 'apple';
+  const words = [
+    'apple',
+    'sky',
+    'blue',
+    'middle',
+    'set',
+  ]
+  let word = words[Math.floor(Math.random() * words.length)];
   let loc = 0;
   let score = 0;
   let miss = 0;
@@ -11,10 +18,24 @@
   const missLabel = document.getElementById('miss');
   target.textContent = word;
 
+
+  function updateTarget() {
+    let placeholder = '';
+    for (let i = 0; i < loc; i++) {
+      placeholder += '_';
+    }
+    target.textContent = placeholder + word.substring(loc);
+  }
+
   window.addEventListener('keydown', e => {
     console.log(e.key);
     if (e.key == word[loc]) {
       loc++;
+      if (loc === word.length) {
+        word = words[Math.floor(Math.random() * words.length)]
+        loc = 0;
+      }
+      updateTarget();
       score++;
       scoreLabel.textContent = score;
     } else {
